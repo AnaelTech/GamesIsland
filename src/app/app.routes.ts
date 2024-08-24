32d3075183b3d050ae5c38e77744cfc3552eabc5
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-08-24 15:49:13 +0200

    add sidebar & home-connexion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { HeroComponent } from './landing/hero/hero.component';
 import { FormInscriptionComponent } from './form-inscription/form-inscription.component';
 import { FormConnexionComponent } from './form-connexion/form-connexion.component';
+import { Component } from '@angular/core';
+import { HomeComponent } from './main/home/home.component';
+import { ContentHomeComponent } from './main/content-home/content-home.component';
 
 
 export const routes: Routes = [
@@ -17,4 +20,15 @@ export const routes: Routes = [
         path: 'connexion',
         component: FormConnexionComponent
     },
+
+    {
+        path: 'home',
+        component: HomeComponent,
+        children: [
+            {
+                path: '',
+                component: ContentHomeComponent
+            }
+        ]
+    }
 ];
