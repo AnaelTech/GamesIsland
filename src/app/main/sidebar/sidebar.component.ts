057b4cf1e0969d6cb37c931837fec1e4fa178c1f
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-09-10 15:15:39 +0200

    refonte main

--- src/app/main/sidebar/sidebar.component.ts
+++ src/app/main/sidebar/sidebar.component.ts
@@ -9,18 +9,4 @@ import { RouterLink } from '@angular/router';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  isSidebarOpen = false;
-  private elementRef= inject(ElementRef);
-
-  toggleSidebar() {
-    this.isSidebarOpen = !this.isSidebarOpen;
-  }
-
-  @HostListener('document:click', ['$event'])
-  onClickOutside(event: Event) {
-    const clickedInside = this.elementRef.nativeElement.contains(event.target);
-    if (!clickedInside) {
-      this.isSidebarOpen = false;
-    }
-  }
 }
