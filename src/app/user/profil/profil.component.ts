3e4e4a6403d89b4954dc4c70f0f60df445062b0e
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-09-21 13:17:58 +0200

    update profil user

--- src/app/user/profil/profil.component.ts
+++ src/app/user/profil/profil.component.ts
@@ -1,8 +1,9 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Base64 } from 'js-base64';
-import { User } from '../../entity';
+import { Game, User } from '../../entity';
 import { UserService } from '../../shared/user.service';
+import { GameService } from '../../shared/game.service';
 
 @Component({
   selector: 'app-profil',
@@ -15,9 +16,13 @@ export class ProfilComponent implements OnInit {
 
   public user: User | undefined;
 
+  public games: Game[] = [];
+
   private route: ActivatedRoute = inject(ActivatedRoute);
 
   private userService: UserService = inject(UserService);
+
+  private gameService: GameService = inject(GameService);
 
   
 
@@ -32,13 +37,11 @@ export class ProfilComponent implements OnInit {
       const encodedId = params.get('id');
       if (encodedId) {
         const id = Base64.decode(encodedId);
-        console.log(id);
         this.userService.getUser(id).subscribe((data: User) => {
+          console.log(data.wishLists);
           this.user = data;
-          console.log(this.user);
         });
       }
     });
   }
-
 }
